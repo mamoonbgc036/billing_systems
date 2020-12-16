15cb9481be1ad2e46ee274a9ad849cfac7d317f3
Abdullah Al Mamoon <mamoonbgc0607@gmail.com>
2020-12-16 21:50:41 +0600

    16/12/20 9.50pm

---
+++ js/app.js
@@ -0,0 +1,27 @@
+	$(document).ready(function(e){
+			let total = 0;
+			$(document).on('keypress', ()=>{
+				if (event.keyCode == 13) {
+					calculate();
+				}
+			});
+
+			$('#addItem').on('click', ()=>{
+				calculate();
+			})
+
+			function calculate(){
+				let name = $('#item').val();
+				let price = $('#price').val();
+				let qty = $('#qty').val();
+				let subTotal = price*qty;
+				$('#add').append(`<tr>
+					<td>`+name+`</td>
+					<td>`+qty+`</td>
+					<td>`+price+`</td>
+					<td id="rowTotal">`+subTotal+`</td>
+				</tr>`);
+				total += subTotal;
+				$('#subTotal').html(total);
+			}
+		})
